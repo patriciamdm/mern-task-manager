1db488b653050b72d1cbc1fa7efaf2a5e46a6ca9
Paricia Muñoz <patrimdm96@gmail.com>
2021-03-18 21:53:05 +0100

    implementing json web token

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,27 +1,49 @@
 const bcrypt = require("bcrypt")
+const jwt = require('jsonwebtoken')
+const { validationResult } = require('express-validator')
 
 const User = require('../models/user.model')
 
 
 exports.createUser = async (req, res) => {
 
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ msgs: errors.array() })
+    }
+
     const {username, email, password, registered} = req.body
 
     try {
+
         const prevUser = await User.findOne({ email })
 
         if (prevUser) {
             return res.status(400).json({ msg: 'User already exists' })
         }
 
+        
         const salt = await bcrypt.genSaltSync(10)
         const hashPass = bcrypt.hashSync(password, salt)
 
-        await User.create({ username, email, password: hashPass, registered })
-        res.json({ msg: 'User created successfully' })
+        const user = await User.create({ username, email, password: hashPass, registered })
+
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, process.env.TOKEN, {
+            expiresIn: 3600 // 1 hour
+        }, (err, token) => {
+            if (err) throw err
+            res.json({ token })
+        })
         
     } catch (err) {
-        
+
         console.error('Error creating user:', err)
         res.status(500).json({ msg: 'Error saving user to database' })
     }
--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const {check} = require('express-validator')
 
 const userController = require('../controllers/userController.js')
 
@@ -16,6 +17,13 @@ const userController = require('../controllers/userController.js')
 // })
 
 
-router.post('/', userController.createUser)
+router.post('/',
+    [
+        check('username', 'Username is required').not().isEmpty(),
+        check('email', 'Please, include a valid email').isEmail(),
+        check('password', 'Password length must be at least 6 characters').isLength({ min: 6 })
+    ],
+    userController.createUser
+)
 
 module.exports = router
